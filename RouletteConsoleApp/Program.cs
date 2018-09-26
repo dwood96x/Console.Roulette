318b769738bfc7f54234891961af11b96bcb6cee
David Wood <dwood96x@gmail.com>
2018-09-26 15:53:13 -0400

    Evends/Odds are coded in

--- RouletteConsoleApp/Program.cs
+++ RouletteConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace RouletteConsoleApp
 {
@@ -10,6 +11,51 @@ namespace RouletteConsoleApp
     {
         static void Main(string[] args)
         {
+            Menu();
+        }
+        public static void Menu()
+        {
+            RouletteLogic roulette = new RouletteLogic();
+            int[] WheelArray = new int[38];
+            for (int i = -1; i <= 36; i++)
+            {
+                WheelArray[i + 1] = i;
+            }
+        }
+    }
+    class RouletteLogic
+    {
+        public int SpinTheWheel(int[] Wheel)
+        {
+            Random rng = new Random();
+            int result = rng.Next(Wheel.Min(), Wheel.Max());
+            return result;
+        }
+        /*
+        DONE 1. Numbers: the number of the bin
+        DONE 2. Evens/Odds: even or odd numbers
+        3. Reds/Blacks: red or black colored numbers
+        4. Lows/Highs: low (1 { 18) or high (19 { 38) numbers.
+        5. Dozens: row thirds, 1 { 12, 13 { 24, 25 { 36
+        6. Columns: rst, second, or third columns
+        7. Street: rows, e.g., 1/2/3 or 22/23/24
+        8. 6 Numbers: double rows, e.g., 1/2/3/4/5/6 or 22/23/24/25/26/26
+        9. Split: at the edge of any two contiguous numbers, e.g., 1/2, 11/14, and 35/36
+        10. Corner: at the intersection of any four contiguous numbers, e.g., 1/2/4/5, or 23/24/26/27
+         */
+        public void WinningBets(int rollednumber)
+        {
+            Console.WriteLine($"If you placed a bet on {rollednumber}, 35:1 payout!");
+            // TODO : Figure out if 00 or 0 gives a odd or even
+            if (rollednumber % 2 == 0)
+            {
+                Console.WriteLine("If you placed a bet on evens, you won 1:1 payout.");
+            }
+            else
+            {
+                Console.WriteLine("If you placed a bet on odds, you won 1:1 payout.");
+            }
+
         }
     }
 }
