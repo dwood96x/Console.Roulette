725798d7e33a731fbed6d645f19728f0052b028e
David Wood <dwood96x@gmail.com>
2018-09-27 08:43:24 -0400

    Added blacks/reds bet.

--- RouletteConsoleApp/Program.cs
+++ RouletteConsoleApp/Program.cs
@@ -34,7 +34,7 @@ namespace RouletteConsoleApp
         /*
         DONE 1. Numbers: the number of the bin
         DONE 2. Evens/Odds: even or odd numbers
-        3. Reds/Blacks: red or black colored numbers
+        DONE 3. Reds/Blacks: red or black colored numbers
         4. Lows/Highs: low (1 { 18) or high (19 { 38) numbers.
         5. Dozens: row thirds, 1 { 12, 13 { 24, 25 { 36
         6. Columns: rst, second, or third columns
@@ -55,6 +55,20 @@ namespace RouletteConsoleApp
             {
                 Console.WriteLine("If you placed a bet on odds, you won 1:1 payout.");
             }
+            // The following code is a array that only contains reds
+            if (new int[] { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36}.Contains(rollednumber))
+            {
+                Console.WriteLine("If you placed a bet on reds, you won 1:1 payout");
+            }
+            else if (rollednumber == -1 || rollednumber == 0)
+            {
+                //Do nothing
+            }
+            else
+            {
+                Console.WriteLine("If you placed a bet on blacks, you won 1:1 payout");
+            }
+
 
         }
     }
